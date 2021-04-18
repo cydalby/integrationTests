3f0b4f621e295c3f37929d7fb8e76236eb96dadb
Cy Dalby <cy.dalby@skybettingandgaming.com>
2021-04-18 17:51:25 +0100

    adding simulator tests

--- tests/Simulator.test.ts
+++ tests/Simulator.test.ts
@@ -4,13 +4,13 @@ describe("Simulator", () => {
   beforeEach(async () => {
     await page.goto(`chrome-extension://${config.exId}/index.html`);
     await page.click("#Simulator");
-  });
 
-  it("Blur Component has default value of 2", async () => {
     const blurBtn = await page.$x('//button[text()="Blur"]');
 
     await blurBtn[0].click();
+  });
 
+  it("Blur Component has default value of 2", async () => {
     const blurElement = await page.$("input[type=range]");
     const value = await blurElement?.getProperty("value");
 
@@ -18,10 +18,14 @@ describe("Simulator", () => {
   });
 
   it("Change Blur Component value to 8", async () => {
-    const blurBtn = await page.$x('//button[text()="Blur"]');
-
-    await blurBtn[0].click();
+    await page.evaluate(() => {
+      return ((document as any).querySelector("input[type=range]").value = 8);
+    });
 
     const blurElement = await page.$("input[type=range]");
+
+    const value = await blurElement?.getProperty("value");
+
+    expect(await value?.jsonValue()).toEqual("8");
   });
 });
